bbc48ccdde675437138a55b8b7f833ae467724ad
根荣 许 <1589575776@qq.com>
2023-02-21 12:41:59 +0800

    java

---
+++ hello_world.java
@@ -0,0 +1,6 @@
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("file.encoding"));
+        System.out.println("Hello World!一二三");
+    }
+}
