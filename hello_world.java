b77ef4c7c35715161c157a7cfca90cde5bea103c
根荣 许 <1589575776@qq.com>
2023-03-09 17:12:13 +0800

    添加时间

--- hello_world.java
+++ hello_world.java
@@ -2,5 +2,7 @@ public class hello_world {
     public static void main(String[] args) {
         System.out.println(System.getProperty("file.encoding"));
         System.out.println("Hello World!一二三");
+        //输出当前时间
+        System.out.println(System.currentTimeMillis());
     }
 }
